0fe2b6de568737b04e6c34f91cab634744ccec70
崔庆才丨静觅 <cqc@cuiqingcai.com>
2021-12-29 23:49:43 +0800

    Update spider3.py

--- spider3.py
+++ spider3.py
@@ -11,8 +11,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
-INDEX_URL = 'https://dynamic5.scrape.cuiqingcai.com/api/book/?limit=18&offset={offset}'
-DETAIL_URL = 'https://dynamic5.scrape.cuiqingcai.com/api/book/{id}'
+INDEX_URL = 'https://spa5.scrape.center/api/book/?limit=18&offset={offset}'
+DETAIL_URL = 'https://spa5.scrape.center/api/book/{id}'
 PAGE_SIZE = 18
 PAGE_NUMBER = 1
 CONCURRENCY = 5
